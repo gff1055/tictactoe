da4c8e252db15e46f1fe4202b335a77e37958079
gff1055 <guilhermeferreirafb@gmail.com>
2021-07-21 14:50:27 -0300

    indicacao de vencedor

--- script.js
+++ script.js
@@ -15,34 +15,137 @@ const JOGADOR1 = 'X';
 const JOGADOR2 = 'O';
 
 let jogador = JOGADOR1;	// O jogador do turno
+let flagVencedor = false;
 
-// Funcao que contem a logica do jogo
-function jogar(event){			
-	
+
+
+function marcarTabuleiro(espaco){
 	// Cria uma copia do objeto tabuleiro que terá a propriedade modificada
 	tabuleiro = Object.assign({}, tabuleiro, {
-		[event.target.id]: jogador						// redefine a propriedade do objeto no novo tabuleiro
+		[event.target.id]: jogador					// redefine a propriedade do objeto no novo tabuleiro
 	});
-
-	event.target.classList.add('marcador-' + jogador);	// Preenchendo a posicao no tabuleiro com "X" ou "O"
+	espaco.classList.add('marcador-' + jogador);	// Preenchendo a posicao no tabuleiro com "X" ou "O"
 }
 
-Object.keys(tabuleiro).forEach(posicao => {
-	const espaco = document.getElementById(posicao);			// Obtendo elemento por ID
-	espaco.addEventListener('click', jogar);							// Adicionando Event Listener de click
+
+
+function mudaTurno(){
+	if(jogador === JOGADOR1)
+		jogador = JOGADOR2;
+	else
+		jogador = JOGADOR1;
+}
+
+
+
+
+function checarVitoria(){
+	
+	if(!flagVencedor){
+		sufixoVencedor = ['-acima', '', '-abaixo'].find(sufixo => {
+			if(tabuleiro['esquerda' + sufixo] === jogador && 
+				tabuleiro['centro' + sufixo] === jogador &&
+				tabuleiro['direita' + sufixo] === jogador){
+				
+				flagVencedor = [
+					'esquerda' + sufixo,
+					'centro' + sufixo,
+					'direita' + sufixo
+				];
+			}
+		});
+
+		['esquerda', 'centro', 'direita'].find(sufixo => {
+			if(tabuleiro[sufixo + '-acima'] === jogador && 
+				tabuleiro[sufixo] === jogador &&
+				tabuleiro[sufixo + '-abaixo'] === jogador){
+
+				flagVencedor = [
+					sufixo + '-acima',
+					sufixo,
+					sufixo + '-abaixo'
+				];
+			}
+		});
+
+		if(tabuleiro['esquerda-acima'] === jogador && 
+			tabuleiro['centro'] === jogador &&
+			tabuleiro['direita-abaixo'] === jogador){
+
+				flagVencedor = [
+					'esquerda-acima',
+					'centro',
+					'direita-abaixo'
+				];
+			}
+
+		if(tabuleiro['direita-acima'] === jogador && 
+				tabuleiro['centro'] === jogador &&
+				tabuleiro['esquerda-abaixo'] === jogador){
+				flagVencedor = [
+					'direita-acima',
+					'centro',
+					'esquerda-abaixo'
+				];
+		}
+
+		//console.log(flag);
+		return flagVencedor;
+	}
+}
+
+
+
+// Funcao que contem a logica do jogo
+function jogar(event){
+	const espaco = event.target;
+
+	// Checar se o espaço nao esta preenchido
+	if(tabuleiro[espaco.id] !== null){
+		return;
+	}
+
+	
+	//marcarTabuleiro(espaco);
+	if(!flagVencedor) marcarTabuleiro(espaco);
+	
+
+	if(checarVitoria()){
+		vencedor = jogador;
+		const mensagem = document.getElementById('mensagem');
+		
+		mensagem.textContent = "Vitoria ao jogador " + jogador;
+		
+		flagVencedor.forEach(posicao =>{
+			const espaco = document.getElementById(posicao);
+			espaco.style.backgroundColor = '#008800';
+			espaco.style.color = '#ffffff';
+		});	
+	}
+	
+	
+	mudaTurno();
+
+}
+
+
+
+// Obtendo todas as propriedades do objeto tabuleiro 
+Object.keys(tabuleiro).forEach(posicao => {						// Para cada propriedade(posicao) do objeto tabuleiro
+	const espaco = document.getElementById(posicao);			// Obtem elemento por ID
+	espaco.addEventListener('click', jogar);					// Adiciona Event Listener de click
 });
 
 
 
-/*function mudaTurno(atual){
-	if(atual === JOGADOR1)
-		return JOGADOR2;
-	else
-		return	JOGADOR1;
-	
-}
 
-function marcarTabuleiro(espaco){
+
+
+
+
+
+
+/*function marcarTabuleiro(espaco){
 	tabuleiro = Object.assign({}, tabuleiro, {							// Faz a copia do tabuleiro para efetuar a jogada
 		[espaco.id]: jogador										// A jogada é efetuada na copia feita
 	});
